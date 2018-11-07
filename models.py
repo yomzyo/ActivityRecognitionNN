17762e89d7ca4c3fb983fa1f4cf3688f4943ff8a
yomzyo <yomzyo@gmail.com>
2018-11-06 20:29:29 -0500

    Model for LSTM + CNN created

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
-from keras.models import Sequential
-from keras.layers import LSTM, Dense, Dropout
+from keras.models import Sequential, Model
+from keras.layers import LSTM, Dense, Dropout, Input, Embedding
+from keras.layers import Conv2D, MaxPooling2D, Flatten
+from keras.layers import concatenate
 
 
 class ThreeLayerLSTM():
@@ -16,14 +18,70 @@ class ThreeLayerLSTM():
         model = Sequential()
         model.add(LSTM(self.L1, return_sequences=True,
                        input_shape=(self.t, self.data_dim),
-                       activation='tanh'))
+                       activation='relu'))
         # model.add(Dropout(0.5))
-        model.add(LSTM(self.L2, return_sequences=True, activation='tanh'))
+        model.add(LSTM(self.L2, return_sequences=True, activation='relu'))
         # model.add(Dropout(0.5)
-        model.add(LSTM(self.L3, activation='tanh'))
+        model.add(LSTM(self.L3, activation='relu'))
         model.add(Dense(self.num_classes, activation='softmax'))
         model.compile(loss='categorical_crossentropy',
                       optimizer='Adam',
                       metrics=['accuracy'])
 
+        return model
+
+
+class ThreeLayerLSTMandCNN():
+    def __init__(self, L1=500, L2=500, L3=500, t=1000,
+                 num_classes=10, data_dim=36, imgHeight=540, imgWidth=960):
+        self.L1 = L1
+        self.L2 = L2
+        self.L3 = L3
+        self.data_dim = data_dim
+        self.t = t
+        self.num_classes = num_classes
+        self.imgWidth = imgWidth
+        self.imgHeight = imgHeight
+
+    def build_network(self):
+        # Build 3 LSTM layers for temporal recognition
+        temporal_model = Sequential()
+        temporal_model.add(LSTM(self.L1, return_sequences=True,
+                           input_shape=(self.t, self.data_dim),
+                           activation='relu'))
+        # model.add(Dropout(0.5))
+        temporal_model.add(LSTM(self.L2, return_sequences=True,
+                                activation='relu'))
+        # model.add(Dropout(0.5)
+        temporal_model.add(LSTM(self.L3, activation='relu'))
+
+        # Skeleton sequence output
+        temporal_input = Input(shape=(self.t, self.data_dim))
+        encoded_temp = temporal_model(temporal_input)
+
+        # build spatial network
+        spat_model = Sequential()
+        spat_model.add(Conv2D(64, (3, 3), activation='relu', padding='same',
+                       input_shape=(self.imgHeight, self.imgWidth, 3)))
+        spat_model.add(Conv2D(64, (3, 3), activation='relu'))
+        spat_model.add(MaxPooling2D(2, 2))
+        spat_model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+        spat_model.add(Conv2D(128, (3, 3), activation='relu'))
+        spat_model.add(MaxPooling2D(2, 2))
+        spat_model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+        spat_model.add(Conv2D(256, (3, 3), activation='relu'))
+        spat_model.add(Conv2D(256, (3, 3), activation='relu'))
+        spat_model.add(MaxPooling2D(2, 2))
+        spat_model.add(Flatten())
+
+        spat_input = Input(shape=(self.imgHeight, self.imgWidth, 3))
+        encoded_spat = spat_model(spat_input)
+
+        merged = concatenate([encoded_temp, encoded_spat])
+
+        output = Dense(self.num_classes, activation='softmax')(merged)
+        model = Model(inputs=[temporal_input, spat_input], outputs=output)
+        model.compile(loss='categorical_crossentropy',
+                      optimizer='Adam',
+                      metrics=['accuracy'])
         return model
